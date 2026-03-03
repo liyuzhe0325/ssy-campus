9efbe8455a019e616281a789d6151b5558da1791
liyuzhe0325 <3466335414@qq.com>
2026-03-03 15:17:28 +0800

    Create toastStore.ts

---
+++ src/store/toastStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+
+export type ToastType = 'success' | 'error' | 'info' | 'warning';
+
+interface Toast {
+  id: string;
+  type: ToastType;
+  message: string;
+  duration?: number;
+}
+
+interface ToastState {
+  toasts: Toast[];
+  addToast: (toast: Omit<Toast, 'id'>) => void;
+  removeToast: (id: string) => void;
+  clearToasts: () => void;
+}
+
+export const useToastStore = create<ToastState>((set) => ({
+  toasts: [],
+  addToast: (toast) => {
+    const id = Date.now().toString(36) + Math.random().toString(36).substr(2);
+    set((state) => ({
+      toasts: [...state.toasts, { ...toast, id }],
+    }));
+    if (toast.duration !== 0) {
+      setTimeout(() => {
+        set((state) => ({
+          toasts: state.toasts.filter((t) => t.id !== id),
+        }));
+      }, toast.duration || 3000);
+    }
+  },
+  removeToast: (id) =>
+    set((state) => ({
+      toasts: state.toasts.filter((t) => t.id !== id),
+    })),
+  clearToasts: () => set({ toasts: [] }),
+}));
